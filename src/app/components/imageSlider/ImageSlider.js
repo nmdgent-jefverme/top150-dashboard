25beaa1c33171729f06f8fa1dd566449d60d20d3
nmdgent-jefverme <jefvermeireown@gmail.com>
2020-03-29 12:59:47 +0200

    init repo

---
+++ src/app/components/imageSlider/ImageSlider.js
@@ -0,0 +1,28 @@
+import { default as React } from 'react'
+import ImageGallery from 'react-image-gallery';
+import '../../../../node_modules/react-image-gallery/styles/css/image-gallery.css';
+
+const images = [
+    {
+      original: 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    },
+    {
+      original: 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    },
+  ];
+
+const ImageSlider = () => {
+    return(
+        <div>
+            <ImageGallery items={images} autoPlay={true} showThumbnails={false}/>
+        </div>
+    )
+}
+
+export default ImageSlider;

7b7e9e5784cd930345213402d6b6d5e16b26aac0
nmdgent-jefverme <jefvermeireown@gmail.com>
2020-03-29 15:45:51 +0200

    wip

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './App.css';
 import { ImageSlider } from './components';
 
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
-    <div className="App">
-      <ImageSlider />
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <div className="image__container">
+              <ImageSlider />
+            </div>
+          </div>
+          <div className="col-sm">
+            <iframe className="spotify__container" title="spotify playlist" src="https://open.spotify.com/embed/album/2FeyIYDDAQqcOJKOKhvHdr" height="auto" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
